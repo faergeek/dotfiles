77c2e8fab2b223b43b938ce2348e31681210795b
Sergey Slipchenko <faergeek@gmail.com>
2019-01-24 08:41:01 +0300

    Cleanup config a bit

--- omf/init.fish
+++ omf/init.fish
@@ -9,23 +9,3 @@ set -g theme_display_k8s_context yes
 # set nvim as an editor
 set -gx EDITOR nvim
 set -gx VISUAL nvim
-
-# colors for man pages
-set -gx LESS_TERMCAP_mb (printf "\033[01;31m")
-set -gx LESS_TERMCAP_md (printf "\033[01;31m")
-set -gx LESS_TERMCAP_me (printf "\033[0m")
-set -gx LESS_TERMCAP_se (printf "\033[0m")
-set -gx LESS_TERMCAP_so (printf "\033[01;44;33m")
-set -gx LESS_TERMCAP_ue (printf "\033[0m")
-set -gx LESS_TERMCAP_us (printf "\033[01;32m")
-
-# set paths for gnu binaries and man pages
-set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-
-# pkgconfig path for XQuartz
-set -gx PKG_CONFIG_PATH /opt/X11/lib/pkgconfig
-# pkgconfig path for zlib
-set -gx PKG_CONFIG_PATH /usr/local/opt/zlib/lib/pkgconfig:$PKG_CONFIG_PATH
-
-# set locale
-set -gx LANG ru_RU.UTF-8
