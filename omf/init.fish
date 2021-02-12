39f51c710f4f2c2193d51cf671c6bfa173a9d06a
Sergey Slipchenko <faergeek@ya.ru>
2021-02-12 22:54:01 +0300

    Add platform-tools to PATH manually

--- omf/init.fish
+++ omf/init.fish
@@ -10,4 +10,5 @@ set -g theme_display_k8s_context yes
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+set -gx PATH ~/Library/Android/sdk/platform-tools $PATH
 set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
