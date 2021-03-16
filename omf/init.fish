644236f733e838474013f1038aa404ad55fb6085
Sergey Slipchenko <faergeek@ya.ru>
2021-03-16 06:07:33 +0300

    Revert "Show node version in prompt"
    This reverts commit 070879d4248cee6f1ce61702e5976c7681a7f9db.

--- omf/init.fish
+++ omf/init.fish
@@ -5,7 +5,6 @@ set -g theme_title_display_process yes
 set -g theme_show_exit_status yes
 set -g theme_newline_cursor yes
 set -g theme_display_k8s_context yes
-set -g theme_display_node yes
 set -g theme_color_scheme dracula
 
 # set nvim as an editor
