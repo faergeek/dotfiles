eaaeaa54f2f3c449efbb4fcc2a240845d239cf8b
Sergey Slipchenko <faergeek@gmail.com>
2023-02-01 09:55:16 +0400

    Add glsl filetype to plenary

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -2,6 +2,8 @@ local telescope = require 'telescope'
 local builtin = require 'telescope.builtin'
 local themes = require 'telescope.themes'
 
+require('plenary.filetype').add_file 'glsl'
+
 telescope.setup {
   defaults = {
     dynamic_preview_title = true,
---
+++ neovim/.config/nvim/data/plenary/filetypes/glsl.lua
@@ -0,0 +1,5 @@
+return {
+  extension = {
+    ['glsl'] = 'glsl',
+  },
+}
