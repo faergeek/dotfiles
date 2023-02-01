301d6adad4f4ceaa59d7536a2b3426101288a252
Sergey Slipchenko <faergeek@gmail.com>
2023-02-01 13:40:39 +0400

    Only use local prettier installation

--- neovim/.config/nvim/after/plugin/mason.lua
+++ neovim/.config/nvim/after/plugin/mason.lua
@@ -86,13 +86,23 @@ require('mason-lspconfig').setup_handlers {
 }
 
 require('mason-null-ls').setup {
-  automatic_setup = true,
   ensure_installed = {
-    'prettierd',
+    'prettier',
     'stylua',
   },
 }
 
-require('null-ls').setup()
+local null_ls = require 'null-ls'
 
-require('mason-null-ls').setup_handlers {}
+require('mason-null-ls').setup_handlers {
+  function(source_name, methods)
+    require 'mason-null-ls.automatic_setup'(source_name, methods)
+  end,
+  prettier = function()
+    null_ls.register(null_ls.builtins.formatting.prettier.with {
+      only_local = 'node_modules/.bin',
+    })
+  end,
+}
+
+null_ls.setup()
