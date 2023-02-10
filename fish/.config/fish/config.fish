0110cefa79fab104346e0f669bdb8a4e20761b93
Sergey Slipchenko <faergeek@gmail.com>
2023-02-10 08:59:05 +0400

    Add tealdeer config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
+  set -gx TEALDEER_CONFIG_DIR ~/.config/tealdeer
+
   if type pfetch 2&> /dev/null
     function fish_greeting
       PF_INFO="ascii os shell editor memory uptime palette" pfetch
