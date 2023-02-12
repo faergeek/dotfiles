5ca9df6b2a4d24ce9946cf76e59fdabe4ea20be4
Sergey Slipchenko <faergeek@gmail.com>
2023-02-12 18:27:52 +0400

    fish: configure pfetch to show a bit more info

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,10 @@
 if status is-interactive
   set -gx TEALDEER_CONFIG_DIR ~/.config/tealdeer
+  set -gx PF_INFO "ascii os host kernel shell editor uptime pkgs memory palette"
 
   if type pfetch 2&> /dev/null
     function fish_greeting
-      PF_INFO="ascii os shell editor memory uptime palette" pfetch
+      pfetch
     end
   end
 end
