27738e6a907e7be418e1c257774f4b543e2a6939
Sergey Slipchenko <faergeek@gmail.com>
2023-08-13 13:48:15 +0400

    neovim: use plain old prettier from node_modules

--- neovim/.config/nvim/lua/plugins/formatting.lua
+++ neovim/.config/nvim/lua/plugins/formatting.lua
@@ -6,26 +6,65 @@ return {
       local formatters = require 'format-on-save.formatters'
 
       local eslint = formatters.lsp { client_name = 'eslint' }
-      local prettier = formatters.prettierd
+
+      local function expand_node_bin(bin, ...)
+        local relative_path =
+          vim.fs.normalize('node_modules/.bin/' .. bin, { expand_env = false })
+
+        local results = vim.fs.find(relative_path, {
+          path = vim.fn.expand '%:p:h',
+          type = 'file',
+          upward = true,
+        })
+
+        local node_bin_path = results[1]
+
+        if node_bin_path then
+          return {
+            mode = 'shell',
+            expand_executable = false,
+            cmd = { node_bin_path, ... },
+          }
+        end
+      end
+
+      local function prettier()
+        return expand_node_bin('prettier', '--stdin-filepath', '%')
+      end
+
+      local function list(...)
+        local items = { ... }
+
+        return function()
+          local result = {}
+
+          for _, item in ipairs(items) do
+            if type(item) == 'function' then item = item() end
+
+            table.insert(result, item)
+          end
+
+          return result
+        end
+      end
 
       return {
-        formatter_by_ft = {
-          css = { prettier },
-          dune = { formatters.shell { cmd = { 'dune', 'format-dune-file' } } },
-          html = { prettier },
-          javascript = { eslint, prettier },
-          javascriptreact = { eslint, prettier },
-          json = { prettier },
-          jsonc = { prettier },
-          lua = { formatters.stylua },
-          markdown = { prettier },
-          typescript = { eslint, prettier },
-          typescriptreact = { eslint, prettier },
-          yaml = { prettier },
-        },
-
         fallback_formatter = {
           formatters.lsp {},
+        },
+        formatter_by_ft = {
+          css = list(prettier),
+          dune = { formatters.shell { cmd = { 'dune', 'format-dune-file' } } },
+          html = list(prettier),
+          javascript = list(eslint, prettier),
+          javascriptreact = list(eslint, prettier),
+          json = list(prettier),
+          jsonc = list(prettier),
+          lua = { formatters.stylua },
+          markdown = list(prettier),
+          typescript = list(eslint, prettier),
+          typescriptreact = list(eslint, prettier),
+          yaml = list(prettier),
         },
       }
     end,
