64647b9346ba053e963f0a96551f159e78699d21
Sergey Slipchenko <faergeek@gmail.com>
2023-12-25 20:26:23 +0400

    neovim: tweak qf formatting

--- neovim/.config/nvim/lua/utils/init.lua
+++ neovim/.config/nvim/lua/utils/init.lua
@@ -24,46 +24,56 @@ function M.keymap(desc, modes, lhs, rhs, opts)
 end
 
 function M.quickfixtextfunc(info)
-  local limit = 31
-
-  local function format_item(item)
-    if item.valid == 1 then
-      local fname = ''
-
-      if item.bufnr > 0 then
-        fname = vim.fn.bufname(item.bufnr)
-
-        if fname == '' then
-          fname = '[No Name]'
-        else
-          fname = fname:gsub('^' .. vim.env.HOME, '~')
-        end
-
-        if #fname <= limit then
-          fname = ('%-' .. limit .. 's'):format(fname)
-        else
-          fname = ('…%.' .. (limit - 1) .. 's'):format(fname:sub(1 - limit))
-        end
-      end
-
-      return ('%s |%5d:%-3d|%s %s'):format(
-        fname,
-        item.lnum > 99999 and -1 or item.lnum,
-        item.col > 999 and -1 or item.col,
-        item.type == '' and '' or ' ' .. item.type:sub(1, 1):upper(),
-        item.text
-      )
-    else
-      return item.text
-    end
-  end
-
   local items = info.quickfix == 1
       and vim.fn.getqflist({
         id = info.id,
         items = 0,
       }).items
     or vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+
+  local function get_item_fname(item)
+    local fname = item.bufnr > 0 and vim.fn.bufname(item.bufnr) or ''
+
+    if fname == '' then
+      fname = '[No Name]'
+    else
+      fname = vim.fn.fnamemodify(fname, ':p:.')
+    end
+
+    return fname
+  end
+
+  local fname_limit = 1
+  local lnum_limit = 1
+  local col_limit = 1
+
+  for _, item in ipairs(items) do
+    local fname = get_item_fname(item)
+    local lnum = '' .. item.lnum
+    local col = '' .. item.col
+
+    if #fname > fname_limit then fname_limit = #fname end
+    if #lnum > lnum_limit then lnum_limit = #lnum end
+    if #col > col_limit then col_limit = #col end
+  end
+
+  local function format_item(item)
+    if item.valid == 1 then
+      local fname = get_item_fname(item)
+      local lnum = '' .. item.lnum
+      local col = '' .. item.col
+
+      return ('%s | %s:%s | %s %s'):format(
+        fname .. string.rep(' ', fname_limit - #fname),
+        string.rep(' ', lnum_limit - #lnum) .. lnum,
+        col .. string.rep(' ', col_limit - #col),
+        item.type == '' and '' or ' ' .. item.type:sub(1, 1):upper(),
+        item.text:gsub('^%s+', '')
+      )
+    else
+      return item.text
+    end
+  end
 
   return vim.tbl_map(
     format_item,
