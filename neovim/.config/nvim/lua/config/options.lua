455a3a05a40cec5522465b490d31d09168abb35f
Sergey Slipchenko <faergeek@gmail.com>
2024-01-02 10:57:32 +0400

    neovim: enable treesitter-based folding

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -13,6 +13,8 @@ vim.opt.fillchars = {
   diff = ' ',
 }
 vim.opt.fixeol = false
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldmethod = 'expr'
 vim.opt.guicursor =
   'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait300-blinkon200-blinkoff150'
 vim.opt.ignorecase = true
