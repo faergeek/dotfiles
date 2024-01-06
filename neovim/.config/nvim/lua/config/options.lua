9c846d97118554e7106befcdcfb7762544769568
Sergey Slipchenko <faergeek@gmail.com>
2024-01-06 14:33:51 +0400

    neovim: add linematch to diffopt

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -7,6 +7,7 @@ vim.opt.cursorlineopt = { 'number', 'screenline' }
 vim.opt.diffopt:append {
   'algorithm:patience',
   'indent-heuristic',
+  'linematch:60',
 }
 vim.opt.expandtab = true
 vim.opt.fillchars = {
