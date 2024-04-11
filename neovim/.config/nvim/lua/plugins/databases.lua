af1f403bb3d77332208cb099d16c3301fe0c2f0c
Sergey Slipchenko <faergeek@gmail.com>
2024-04-11 15:15:59 +0400

    neovim: add a keymap for DBUIToggle

--- neovim/.config/nvim/lua/plugins/databases.lua
+++ neovim/.config/nvim/lua/plugins/databases.lua
@@ -11,6 +11,13 @@ return {
       'DBUIAddConnection',
       'DBUILastQueryInfo',
     },
+    keys = {
+      {
+        desc = 'DBUI: Toggle',
+        '<BS>',
+        '<Cmd>DBUIToggle<CR>',
+      },
+    },
     init = function() vim.g.db_ui_use_nerd_fonts = 1 end,
   },
   {
