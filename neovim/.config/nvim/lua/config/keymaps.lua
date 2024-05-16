19d63e77877684f5368a24f87844c69b217d5dee
Sergey Slipchenko <faergeek@gmail.com>
2024-05-16 22:40:42 +0400

    neovim: remove unused loclist keymaps

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -29,17 +29,6 @@ keymap('LSP: Hover', 'n', 'K', vim.lsp.buf.hover)
 keymap('LSP: Rename Symbol', 'n', '<leader>rs', vim.lsp.buf.rename)
 keymap('LSP: Code Action', { 'n', 'x' }, '<leader>a', vim.lsp.buf.code_action)
 
-keymap('LSP: Document Symbols', 'n', 'gO', function()
-  local nr = vim.api.nvim_win_get_number(vim.api.nvim_get_current_win())
-
-  vim.lsp.buf.document_symbol {
-    on_list = function(options)
-      vim.fn.setloclist(nr, {}, ' ', options)
-      vim.cmd.windo { range = { nr }, args = { 'lopen' } }
-    end,
-  }
-end)
-
 keymap('Find References', 'n', '<leader>fr', vim.lsp.buf.references)
 
 keymap('Next quickfix item', 'n', ']q', vim.cmd.cnext)
@@ -47,9 +36,3 @@ keymap('Previous quickfix item', 'n', '[q', vim.cmd.cprevious)
 
 keymap('Next quickfix list', 'n', ']Q', vim.cmd.cnewer)
 keymap('Previous quickfix list', 'n', '[Q', vim.cmd.colder)
-
-keymap('Next loclist item', 'n', ']w', vim.cmd.lnext)
-keymap('Previous loclist item', 'n', '[w', vim.cmd.lprevious)
-
-keymap('Next loclist', 'n', ']W', vim.cmd.lnewer)
-keymap('Previous loclist', 'n', '[W', vim.cmd.lolder)
