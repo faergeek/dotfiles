c6edd4080c6d231824d7f64172cb9db4db50cfc3
Sergey Slipchenko <faergeek@gmail.com>
2024-05-18 11:09:00 +0400

    neovim: don't disable bell for everything

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
+vim.opt.belloff = {
+  'backspace',
+  'cursor',
+  'error',
+  'operator',
+  'showmatch',
+}
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.cpoptions:append 'n'
