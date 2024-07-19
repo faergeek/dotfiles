47533dbe4226b8d877458a43120beac9553f9f11
Sergey Slipchenko <faergeek@gmail.com>
2024-07-19 07:41:56 +0400

    neovim: update packages

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -22,12 +22,6 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-vim.api.nvim_create_user_command(
-  'Browse',
-  function(input) vim.ui.open(input.fargs[1]) end,
-  { nargs = 1 }
-)
-
 require('lazy').setup('plugins', {
   change_detection = {
     notify = false,
