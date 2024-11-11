454a2d0727f9c8f6a7ab3c4ff4f578b516f8d96e
Sergei Slipchenko <faergeek@gmail.com>
2024-11-11 10:55:58 +0400

    kitty: improve scrollback_pager

--- neovim/.config/nvim/lua/utils/init.lua
+++ neovim/.config/nvim/lua/utils/init.lua
@@ -107,4 +107,69 @@ function M.quickfixtextfunc(info)
   )
 end
 
+function M.kitty_scrollback(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN)
+  require('lualine').hide {
+    place = { 'statusline', 'tabline', 'winbar' },
+    unhide = false,
+  }
+
+  vim.opt.cmdheight = 0
+  vim.cmd [[
+    hi! TermCursor guifg=NONE guibg=NONE gui=NONE cterm=NONE
+    hi! TermCursorNC guifg=NONE guibg=NONE gui=NONE cterm=NONE
+  ]]
+  vim.opt.guicursor = 'a:noCursor'
+  vim.opt.laststatus = 0
+  vim.opt.list = false
+  vim.opt.number = false
+  vim.opt.relativenumber = false
+  vim.opt.scrollback = INPUT_LINE_NUMBER + CURSOR_LINE
+  vim.opt.scrolloff = 0
+  vim.opt.signcolumn = 'no'
+
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local line_count = #lines
+
+  local setCursor = function()
+    vim.api.nvim_feedkeys(tostring(INPUT_LINE_NUMBER) .. 'ggzt', 'n', true)
+
+    local line = line_count
+    if CURSOR_LINE <= line then line = CURSOR_LINE end
+
+    vim.api.nvim_feedkeys(tostring(line - 1) .. 'j', 'n', true)
+    vim.api.nvim_feedkeys('0', 'n', true)
+    vim.api.nvim_feedkeys(tostring(CURSOR_COLUMN - 1) .. 'l', 'n', true)
+  end
+
+  local term_buf = vim.api.nvim_create_buf(true, false)
+  local term_io = vim.api.nvim_open_term(term_buf, {})
+
+  for i, line in ipairs(lines) do
+    vim.api.nvim_chan_send(term_io, line)
+
+    if i < line_count then vim.api.nvim_chan_send(term_io, '\r\n') end
+  end
+
+  vim.api.nvim_buf_delete(0, { force = true })
+  vim.api.nvim_win_set_buf(0, term_buf)
+  vim.schedule(setCursor)
+
+  M.autocmd(
+    'Just reset cursor position when trying to enter terminal mode',
+    'ModeChanged',
+    function()
+      local mode = vim.fn.mode()
+      if mode == 't' then
+        vim.cmd.stopinsert()
+        vim.schedule(setCursor)
+      end
+    end,
+    { buffer = 0 }
+  )
+
+  M.keymap('Close on <q>', 'n', 'q', '<Cmd>q<CR>', {
+    buffer = 0,
+  })
+end
+
 return M
