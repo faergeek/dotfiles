ec8a629c8e1ae3edfdcc3f678516cd1f033cda3f
Sergei Slipchenko <faergeek@gmail.com>
2024-11-12 10:22:15 +0400

    neovim: use `telescope.get_cursor` for code actions

--- neovim/.config/nvim/lua/plugins/eyecandy.lua
+++ neovim/.config/nvim/lua/plugins/eyecandy.lua
@@ -33,6 +33,16 @@ return {
       input = {
         start_in_insert = false,
       },
+      select = {
+        get_config = function(opts)
+          if opts.kind == 'codeaction' then
+            return {
+              backend = 'telescope',
+              telescope = require('telescope.themes').get_cursor {},
+            }
+          end
+        end,
+      },
     },
   },
 }
