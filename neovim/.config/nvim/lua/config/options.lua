c9a5da5d130c9d9c7ffbb61b9f8e77d5d31de89e
Sergei Slipchenko <faergeek@gmail.com>
2024-11-16 14:33:18 +0400

    neovim: use vertical bar for command mode

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -19,7 +19,7 @@ vim.opt.foldcolumn = 'auto:9'
 vim.opt.fixeol = false
 vim.opt.foldtext = ''
 vim.opt.guicursor =
-  'n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait300-blinkon200-blinkoff150'
+  'n-v-sm:block,i-c-ci-ve:ver25,r-cr-o:hor20,a:blinkwait300-blinkon200-blinkoff150'
 vim.opt.guifont = 'FantasqueSansM Nerd Font'
 vim.opt.ignorecase = true
 vim.opt.inccommand = 'split'
