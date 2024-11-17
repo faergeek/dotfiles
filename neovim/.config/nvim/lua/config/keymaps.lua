f0c7d5340d31d6df407d1bfa856ec8edff656da9
Sergei Slipchenko <faergeek@gmail.com>
2024-11-17 08:38:35 +0400

    neovim: bring back gO keymap for document symbols

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -22,6 +22,15 @@ keymap('Lazy', 'n', '<leader>l', '<Cmd>Lazy<CR>')
 keymap('LSP: Rename Symbol', 'n', '<leader>rs', vim.lsp.buf.rename)
 keymap('LSP: Code Action', { 'n', 'x' }, '<leader>a', vim.lsp.buf.code_action)
 
+keymap('LSP: Document Symbols', 'n', 'gO', function()
+  vim.lsp.buf.document_symbol {
+    on_list = function(options)
+      vim.fn.setloclist(0, {}, ' ', options)
+      vim.cmd.lopen()
+    end,
+  }
+end)
+
 keymap('Find References', 'n', '<leader>fr', vim.lsp.buf.references)
 keymap('Find Implementations', 'n', '<leader>fi', vim.lsp.buf.implementation)
 
