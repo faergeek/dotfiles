979b3910c63f92256c452cf3466a9509a8713a5a
Sergei Slipchenko <faergeek@gmail.com>
2024-11-20 10:56:50 +0400

    neovim: merge eyecandy.lua into ui.lua

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,52 @@
 return {
   {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      flavour = 'frappe',
+      integrations = {
+        fidget = true,
+        mason = true,
+        native_lsp = {
+          underlines = {
+            errors = { 'undercurl' },
+            hints = { 'undercurl' },
+            warnings = { 'undercurl' },
+            information = { 'undercurl' },
+          },
+        },
+        nvim_surround = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  {
+    'stevearc/dressing.nvim',
+    event = 'VeryLazy',
+    opts = {
+      input = {
+        start_in_insert = false,
+      },
+      select = {
+        get_config = function(opts)
+          if opts.kind == 'codeaction' then
+            return {
+              backend = 'telescope',
+              telescope = require('telescope.themes').get_cursor {},
+            }
+          end
+        end,
+      },
+    },
+  },
+  {
     'stevearc/oil.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     event = 'VeryLazy',
