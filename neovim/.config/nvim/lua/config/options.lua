3e3ac096054b0feb6a9aa3baa60d6ad0dcf3f49d
Sergei Slipchenko <faergeek@gmail.com>
2024-12-04 08:23:24 +0400

    neovim: set clipboard=unnamedplus, use OSC52 only if SSH_TTY is set

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,4 +1,5 @@
 vim.opt.breakindent = true
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.cpoptions:append 'n'
 vim.opt.cursorline = true
@@ -63,6 +64,20 @@ vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.wildignorecase = true
 vim.opt.wildoptions:append { 'fuzzy' }
+
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    },
+  }
+end
 
 vim.g.qf_disable_statusline = true
 vim.g.zig_fmt_autosave = false
