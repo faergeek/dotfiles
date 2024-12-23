4486ccf8bb65466a13744c92a99b413f6886cb9e
Sergey Slipchenko <faergeek@gmail.com>
2024-12-23 11:46:09 +0400

    neovim: add a workaround for 0.10.3 issue with `:Inspect`

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- work around 0.10.3 issue with `:Inspect`
+-- https://github.com/neovim/neovim/issues/31675#issuecomment-2558405042
+vim.hl = vim.highlight
+
 require 'faergeek.config.status'
 require 'faergeek.config.spell'
 require 'faergeek.config.options'
