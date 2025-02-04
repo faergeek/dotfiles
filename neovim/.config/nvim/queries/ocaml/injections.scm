e1f2dae0254127c60bd390bef3e488ffc9b2dd2b
Sergei Slipchenko <faergeek@gmail.com>
2025-02-04 07:09:31 +0400

    neovim: inject javascript into `[%%mel.raw ...]`

---
+++ neovim/.config/nvim/queries/ocaml/injections.scm
@@ -0,0 +1,35 @@
+(item_extension
+  (attribute_id) @_name
+  (#eq? @_name "mel.raw")
+  (attribute_payload
+    (expression_item
+      (quoted_string
+        (quoted_string_content) @injection.content)))
+  (#set! injection.language "javascript"))
+
+(item_extension
+  (attribute_id) @_name
+  (#eq? @_name "mel.raw")
+  (attribute_payload
+    (expression_item
+      (string
+        (string_content) @injection.content)))
+  (#set! injection.language "javascript"))
+
+(extension
+  (attribute_id) @_name
+  (#eq? @_name "mel.raw")
+  (attribute_payload
+    (expression_item
+      (quoted_string
+        (quoted_string_content) @injection.content)))
+  (#set! injection.language "javascript"))
+
+(extension
+  (attribute_id) @_name
+  (#eq? @_name "mel.raw")
+  (attribute_payload
+    (expression_item
+      (string
+        (string_content) @injection.content)))
+  (#set! injection.language "javascript"))
