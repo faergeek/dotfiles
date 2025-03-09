29035e6817ff9bde31534fd39663896b7172d618
Sergei Slipchenko <faergeek@gmail.com>
2025-03-09 10:14:18 +0400

    neovim: remove extra plugin spec entry for plenary.nvim

--- neovim/.config/nvim/lua/faergeek/plugins/init.lua
+++ neovim/.config/nvim/lua/faergeek/plugins/init.lua
@@ -2,5 +2,4 @@ return {
   { 'dstein64/vim-startuptime', cmd = 'StartupTime' },
   { 'browserslist/vim-browserslist', ft = 'browserslist' },
   { 'fladson/vim-kitty', ft = 'kitty' },
-  { 'nvim-lua/plenary.nvim', lazy = true },
 }
