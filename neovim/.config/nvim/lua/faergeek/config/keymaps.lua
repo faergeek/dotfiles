302b67e2df8aad37b782e2f0b5c7dd85e545b840
Sergei Slipchenko <faergeek@gmail.com>
2025-04-03 08:19:26 +0400

    neovim: don't add keymaps for lsp actions having default keymaps

--- neovim/.config/nvim/lua/faergeek/config/keymaps.lua
+++ neovim/.config/nvim/lua/faergeek/config/keymaps.lua
@@ -15,9 +15,3 @@ keymap(
   "v:count ? 'j' : 'gj'",
   { expr = true }
 )
-
-keymap('LSP: Rename Symbol', 'n', '<leader>rs', vim.lsp.buf.rename)
-keymap('LSP: Code Action', { 'n', 'x' }, '<leader>a', vim.lsp.buf.code_action)
-
-keymap('Find References', 'n', '<leader>fr', vim.lsp.buf.references)
-keymap('Find Implementations', 'n', '<leader>fi', vim.lsp.buf.implementation)
