10c9654d5d74118f3621bda4a5abb6c10403fee8
Sergei Slipchenko <faergeek@gmail.com>
2025-04-16 08:02:04 +0400

    neovim: don't set yamlls options which are equal to defaults

--- neovim/.config/nvim/lsp/yamlls.lua
+++ neovim/.config/nvim/lsp/yamlls.lua
@@ -1,8 +1,6 @@
 return {
   settings = {
-    redhat = { telemetry = { enabled = false } },
     yaml = {
-      keyOrdering = false,
       schemaStore = { enable = false, url = '' },
       schemas = require('schemastore').yaml.schemas(),
     },
