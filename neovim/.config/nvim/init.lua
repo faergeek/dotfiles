b8d96f356ba6d27b3f30b0393aecedbc9355a5cc
Sergei Slipchenko <faergeek@gmail.com>
2025-04-18 09:11:16 +0400

    neovim: inline keymaps, autocmds and lazy

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,5 +1,50 @@
 require 'faergeek.config.status'
 require 'faergeek.config.options'
-require 'faergeek.config.keymaps'
-require 'faergeek.config.autocmds'
-require 'faergeek.config.lazy'
+
+vim.keymap.set({ 'n', 'x' }, 'k', "v:count ? 'k' : 'gk'", { expr = true })
+vim.keymap.set({ 'n', 'x' }, 'j', "v:count ? 'j' : 'gj'", { expr = true })
+
+vim.api.nvim_create_autocmd(
+  'TextYankPost',
+  { callback = function() vim.hl.on_yank {} end }
+)
+
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function(event)
+    if vim.bo.buftype == 'help' then
+      vim.keymap.set('n', 'q', '<Cmd>q<CR>', { buffer = event.buf })
+    end
+  end,
+})
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    'https://github.com/folke/lazy.nvim.git',
+    lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('faergeek.plugins', {
+  change_detection = { notify = false },
+  pkg = { sources = { 'lazy', 'packspec' } },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'netrwPlugin',
+        'spellfile',
+        'tarPlugin',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
--- neovim/.config/nvim/lua/faergeek/config/keymaps.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set({ 'n', 'x' }, 'k', "v:count ? 'k' : 'gk'", { expr = true })
-vim.keymap.set({ 'n', 'x' }, 'j', "v:count ? 'j' : 'gj'", { expr = true })
