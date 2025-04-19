747832d7944b405c2624c775722e566ca23c319d
Sergei Slipchenko <faergeek@gmail.com>
2025-04-19 19:20:09 +0400

    neovim: create startup.lua to contain auto-session and alpha

--- neovim/.config/nvim/lua/faergeek/plugins/projects.lua
+++ neovim/.config/nvim/lua/faergeek/plugins/projects.lua
@@ -1,21 +1,3 @@
 return {
   'tpope/vim-dispatch',
-  {
-    'rmagatti/auto-session',
-    lazy = false,
-    keys = {
-      {
-        desc = 'Session: Delete',
-        '<leader>sd',
-        '<Cmd>Autosession delete<CR>',
-      },
-    },
-    ---@module "auto-session"
-    ---@type AutoSession.Config
-    opts = {
-      args_allow_single_directory = false,
-      auto_create = false,
-      use_git_branch = true,
-    },
-  },
 }
