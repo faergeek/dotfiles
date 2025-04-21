f323452647f14e58f31271e8965595828650f8cf
Sergei Slipchenko <faergeek@gmail.com>
2025-04-21 07:44:40 +0400

    neovim: disable signcolumn in help windows

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -12,6 +12,7 @@ vim.api.nvim_create_autocmd(
 vim.api.nvim_create_autocmd('BufReadPost', {
   callback = function(event)
     if vim.bo.buftype == 'help' then
+      vim.opt_local.signcolumn = 'auto'
       vim.keymap.set('n', 'q', '<Cmd>q<CR>', { buffer = event.buf })
     end
   end,
