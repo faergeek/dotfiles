2acaf2f483d9e284778c737d93fcb7221d66b170
Sergei Slipchenko <faergeek@gmail.com>
2025-04-23 08:13:11 +0400

    neovim: load vim-dispatch lazily

--- neovim/.config/nvim/lua/faergeek/plugins/init.lua
+++ neovim/.config/nvim/lua/faergeek/plugins/init.lua
@@ -1,3 +1,32 @@
 return {
-  'tpope/vim-dispatch',
+  {
+    'tpope/vim-dispatch',
+    cmd = {
+      'AbortDispatch',
+      'Copen',
+      'Dispatch',
+      'FocusDispatch',
+      'Make',
+      'Spawn',
+      'Start',
+    },
+    keys = {
+      'm<CR>',
+      'm<Space>',
+      'm!',
+      'm?',
+      '`<CR>',
+      '`<Space>',
+      '`!',
+      '`?',
+      "'<CR>",
+      "'<Space>",
+      "'!",
+      "'?",
+      "g'<CR>",
+      "g'<Space>",
+      "g'!",
+      "g'?",
+    },
+  },
 }
