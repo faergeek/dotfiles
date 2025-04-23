d6095cf4ca6c82a851bc296c4dccacd402032529
Sergei Slipchenko <faergeek@gmail.com>
2025-04-23 18:35:47 +0400

    Revert "neovim: bring spell checking back"
    This reverts commit 333545cbe54383e905bde60158ca724fa4b003ba.

--- neovim/.config/nvim/ftplugin/git.lua
+++ neovim/.config/nvim/ftplugin/git.lua
@@ -1,3 +1,1 @@
-vim.opt_local.spell = false
-
 vim.keymap.set('n', 'q', '<Cmd>q<CR>', { buffer = 0 })
--- neovim/.config/nvim/ftplugin/qf.lua
+++ neovim/.config/nvim/ftplugin/qf.lua
@@ -3,9 +3,7 @@ vim.opt_local.listchars:remove {
   'multispace',
   'trail',
 }
+vim.opt_local.signcolumn = 'auto'
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
-vim.opt_local.signcolumn = 'auto'
-vim.opt_local.spell = false
-
 vim.keymap.set('n', 'q', '<Cmd>q<CR>', { buffer = 0 })
