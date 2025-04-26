3f5b8af8b6094e4932b35967c578fd7a570b51c7
Sergei Slipchenko <faergeek@gmail.com>
2025-04-26 09:47:01 +0400

    neovim: setup snacks.nvim

--- neovim/.config/nvim/lua/faergeek/plugins/init.lua
+++ neovim/.config/nvim/lua/faergeek/plugins/init.lua
@@ -1,5 +1,18 @@
 return {
   {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@module 'snacks'
+    ---@type snacks.Config
+    opts = {
+      bigfile = {
+        notify = false,
+      },
+      quickfile = {},
+    },
+  },
+  {
     'tpope/vim-dispatch',
     cmd = {
       'AbortDispatch',
