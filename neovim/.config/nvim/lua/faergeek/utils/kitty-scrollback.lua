5a5ef876c9d418a5ca643b7b7adba08415f08db6
Sergei Slipchenko <faergeek@gmail.com>
2025-04-27 15:43:38 +0400

    neovim: pipe stdin to term buf only from kitty-scrollback
    Otherwise it breaks usage of neovim as a $MANPAGER

--- neovim/.config/nvim/lua/faergeek/utils/kitty-scrollback.lua
+++ neovim/.config/nvim/lua/faergeek/utils/kitty-scrollback.lua
@@ -1,4 +1,22 @@
 return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN)
+  local buf = vim.api.nvim_get_current_buf()
+  local term_buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_win_set_buf(0, term_buf)
+
+  vim.opt_local.list = false
+  vim.opt_local.number = false
+  vim.opt_local.relativenumber = false
+  vim.opt_local.scrolloff = 0
+  vim.opt_local.signcolumn = 'no'
+
+  vim.api.nvim_chan_send(
+    vim.api.nvim_open_term(term_buf, {}),
+    table.concat(vim.api.nvim_buf_get_lines(buf, 0, -1, false), '\r\n')
+  )
+
+  vim.api.nvim_buf_delete(buf, { force = true })
+
+  vim.keymap.set('n', 'q', '<Cmd>q<CR>', { buffer = term_buf })
   vim.opt.cmdheight = 0
   vim.opt.laststatus = 0
 
