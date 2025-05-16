c66ba6d9cf171a9e6ae2e9a16097acc011ccdb3b
Sergei Slipchenko <faergeek@gmail.com>
2025-05-16 07:57:35 +0400

    neovim: remove snacks.nvim

--- base/dot-config/nvim/lua/faergeek/plugins/init.lua
+++ base/dot-config/nvim/lua/faergeek/plugins/init.lua
@@ -6,19 +6,6 @@ return {
     },
   },
   {
-    'folke/snacks.nvim',
-    priority = 1000,
-    lazy = false,
-    ---@module 'snacks'
-    ---@type snacks.Config
-    opts = {
-      bigfile = {
-        notify = false,
-      },
-      quickfile = {},
-    },
-  },
-  {
     'tpope/vim-dispatch',
     cmd = {
       'AbortDispatch',
