d22551389683ca1382b066a827b011a5de449850
Sergei Slipchenko <faergeek@gmail.com>
2025-09-02 05:03:02 +0400

    neovim: drop nvim-treesitter dependency from nvim-treesitter-context

--- base/.config/nvim/lua/faergeek/plugins/editing.lua
+++ base/.config/nvim/lua/faergeek/plugins/editing.lua
@@ -29,7 +29,6 @@ return {
   },
   {
     'nvim-treesitter/nvim-treesitter-context',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
     opts = {
       max_lines = 8,
       min_window_height = 20,
