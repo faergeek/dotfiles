e0a55770a21ec0d38388f8e66f4e28c4945755ce
Sergei Slipchenko <faergeek@gmail.com>
2026-04-23 08:30:06 +0400

    neovim: always enable `didChangeWatchedFiles.dynamicRegistration`

--- base/.config/nvim/lua/faergeek/config/options.lua
+++ base/.config/nvim/lua/faergeek/config/options.lua
@@ -96,3 +96,10 @@ vim.filetype.add {
 
 vim.hl.priorities.semantic_tokens = 90
 vim.treesitter.language.register('starlark', 'tiltfile')
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = true
+
+vim.lsp.config('*', {
+  capabilities = capabilities,
+})
