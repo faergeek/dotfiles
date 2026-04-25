8ee699db18e5d934e3b5388acbc286f527fa040b
Sergei Slipchenko <faergeek@gmail.com>
2026-04-25 09:21:40 +0400

    neovim: improve python debug adapter configuration

--- base/.config/nvim/lua/faergeek/plugins/debugging.lua
+++ base/.config/nvim/lua/faergeek/plugins/debugging.lua
@@ -119,11 +119,27 @@ return {
         port = 6006,
       }
 
-      dap.adapters.python = {
-        type = 'server',
-        host = '127.0.0.1',
-        port = 5678,
-      }
+      dap.adapters.python = function(cb, config)
+        if config.request == 'attach' then
+          cb {
+            type = 'server',
+            port = (config.connect or config).port or 5678,
+            host = (config.connect or config).host or '127.0.0.1',
+            options = {
+              source_filetype = 'python',
+            },
+          }
+        else
+          cb {
+            type = 'executable',
+            command = config.python,
+            args = { '-m', 'debugpy.adapter' },
+            options = {
+              source_filetype = 'python',
+            },
+          }
+        end
+      end
 
       dap.adapters['pwa-node'] = {
         executable = { command = 'js-debug-adapter' },
