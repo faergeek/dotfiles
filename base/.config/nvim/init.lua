ca041e1217e13c44219efc931efe675ecbab8853
Sergei Slipchenko <faergeek@gmail.com>
2026-06-08 10:02:51 +0400

    Get rid of uwsm

--- base/.config/nvim/init.lua
+++ base/.config/nvim/init.lua
@@ -101,7 +101,6 @@ vim.filetype.add {
     ['.*/%.config/git/config%.local%.example'] = 'gitconfig',
     ['.*/%.config/git/config%.local'] = 'gitconfig',
     ['.*/%.config/swappy/config'] = 'ini',
-    ['.*/%.config/uwsm/env'] = 'sh',
     ['.*/%.vscode/.*%.json'] = 'jsonc',
   },
 }
