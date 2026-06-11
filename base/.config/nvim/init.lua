0ebc2b03effa08a2205e1661ab583f96014e8638
Sergei Slipchenko <faergeek@gmail.com>
2026-06-11 16:07:47 +0400

    Bring uwsm back

--- base/.config/nvim/init.lua
+++ base/.config/nvim/init.lua
@@ -101,6 +101,7 @@ vim.filetype.add {
     ['.*/%.config/git/config%.local%.example'] = 'gitconfig',
     ['.*/%.config/git/config%.local'] = 'gitconfig',
     ['.*/%.config/swappy/config'] = 'ini',
+    ['.*/%.config/uwsm/env'] = 'sh',
     ['.*/%.vscode/.*%.json'] = 'jsonc',
   },
 }
